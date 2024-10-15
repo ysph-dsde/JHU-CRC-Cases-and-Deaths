ae3bf1ff75283a4de0472143793cac260c2c6f04
Shelby Golden <sgolde13@alumni.jh.edu>
2024-10-15 17:41:28 -0400

    Adjust the file name so that two analysis scripts can be generated.

--- Analysis Script_Time-Series Plot.R
+++ Analysis Script_Time-Series Plot.R
@@ -20,42 +20,55 @@ library(curl)
 library(tidyr)
 
 
+## Function to check if an element is not in a vector
+"%!in%" <- function(x,y)!('%in%'(x,y))
+
+
 ## ----------------------------------------------------------------------------
 ## LOAD IN THE DATA
 ## This data is from the COVID-19 Data Repository by the Center for Systems 
 ## Science and Engineering (CSSE) at Johns Hopkins University. We load it in
 ## directly from their GitHub page using the raw URL.
 
-df.url <- "https://raw.githubusercontent.com/govex/COVID-19/refs/heads/master/data_tables/vaccine_data/us_data/time_series/time_series_covid19_vaccine_doses_admin_US.csv"
+df.url <- "https://raw.githubusercontent.com/govex/COVID-19/refs/heads/master/data_tables/vaccine_data/us_data/time_series/time_series_covid19_vaccine_us.csv"
 df     <- read_csv(file = curl(df.url), show_col_types = FALSE) %>%
               as.data.frame()
 
+
 ## View the first and last few rows of the data
-head(df)[, 1:15]
-tail(df)[, 1:15]
+head(df)
+tail(df)
 
-
-## ----------------------------------------------------------------------------
-## DATA PREPARATION
 
 ## Number of rows and columns
 dim(df)
 
 
-## There are 55 US States and Major Territories, so what are the remaining 6?
+## ----------------------------------------------------------------------------
+## DATA PREPARATION
 
+## There are 50 States, 5 Major Territories, and 1 district. 
+## What are the remaining 62 entries for?
+df$Province_State %>% unique() %>%
+      .[. %!in% datasets::state.name]
 
 
 
 ## Check the variable class of each column
-apply(df, 2, class) %>% table()
+apply(df, 2, class)
 
 df[, -c(2, 3, 6, 7, 8, 11)] <- apply(df[, -c(2, 3, 6, 7, 8, 11)], 2, function(x) as.numeric(x))
 
 
-df$Admin2 %>% unique() 
+df$Admin2 %>% unique()
 
 
+
+
+
+
+## Check the vaccination rate is monotonic
+all(df == cummax(df$`2023-03-09`))))
 
 
 
