f4cc7ca44d6f16c76bba63a9f01282ad70ccdc08
Shelby Golden <sgolde13@alumni.jh.edu>
2024-10-16 00:29:07 -0400

    Add the data dictionary, and include the full analysis script. Completed the "Data Cleaning" and "Plot" sections.

--- Analysis Script_Time-Series Plot.R
+++ Analysis Script_Time-Series Plot.R
@@ -18,14 +18,19 @@ renv::restore()
 library(readr)
 library(curl)
 library(tidyr)
+library(dplyr)
+library(ggplot2)
 
 
 ## Function to check if an element is not in a vector
 "%!in%" <- function(x,y)!('%in%'(x,y))
 
 
+
+
 ## ----------------------------------------------------------------------------
 ## LOAD IN THE DATA
+
 ## This data is from the COVID-19 Data Repository by the Center for Systems 
 ## Science and Engineering (CSSE) at Johns Hopkins University. We load it in
 ## directly from their GitHub page using the raw URL.
@@ -44,35 +49,94 @@ tail(df)
 dim(df)
 
 
+
+
 ## ----------------------------------------------------------------------------
 ## DATA PREPARATION
 
-## There are 50 States, 5 Major Territories, and 1 district. 
-## What are the remaining 62 entries for?
-df$Province_State %>% unique() %>%
-      .[. %!in% datasets::state.name]
-
-
-
 ## Check the variable class of each column
-apply(df, 2, class)
-
-df[, -c(2, 3, 6, 7, 8, 11)] <- apply(df[, -c(2, 3, 6, 7, 8, 11)], 2, function(x) as.numeric(x))
+sapply(df, class)     # works better than apply() in this scenario
 
 
-df$Admin2 %>% unique()
+## Check for missing values
+sapply(df, function(x) sum(is.na(x))) %>% 
+        as.data.frame() %>% `colnames<-`("NA Count")
+
+
+## Investigate the NA province
+na_province  = df[df$Province_State %in% NA, ] 
+
+na_province     %>% head()
+na_province$UID %>% unique()
+
+df[df$Province_State %in% NA, "Province_State"] <- "US"
+
+
+## Aggregate the data by month and state/territory
+## We no longer need columns "Date", "UID", or "Country_Region", and
+## so we will remove them from the data frame.
+
+df <- df %>% group_by(month = lubridate::floor_date(Date, "month")) %>% 
+          as.data.frame()
+
+df_byMonth <- aggregate(. ~ month + Province_State, df[, -c(1:2, 4)], sum)
+
+
+## There are 50 States, 5 Major Territories, 1 district, and 1 total.
+unique_provinces = df_byMonth$Province_State %>% unique()
+length(unique_provinces)
+
+
+## We have an excess of 5 rows, which are not states or territories.
+## What are they?
+us_state        = datasets::state.name
+us_territories  = c("American Samoa", "Guam", "Northern Mariana Islands", 
+                    "Puerto Rico", "Virgin Islands")
+us_district     = "District of Columbia"
+
+
+extra_provinces = unique_provinces %>% 
+                      .[. %!in% c(us_state, us_territories, us_district, "US")]
+extra_provinces
+
+
+## Check each province has entries for the same number of months
+## Boolean test is TRUE when all provinces have the same number of months
+table(df_byMonth$Province_State, df_byMonth$month) %>% 
+        sapply(., function(x) x == 1) %>% all()
 
 
 
 
+## ----------------------------------------------------------------------------
+## TIME-SERIES PLOTS
+
+## US states, major territories, and the district of Columbia
+plot_1 <- df_byMonth %>%
+            filter(Province_State != "US" & Province_State %!in% extra_provinces) %>%
+            ggplot(data = ., aes(x = month, y = People_fully_vaccinated)) +
+                  geom_line(aes(color = Province_State)) +
+                  scale_y_continuous(labels = scales::comma) +
+                  labs(title = "People Fully Vaccinated by State/Territory",
+                       x = "Month",
+                       y = "People Fully Vaccinated") +
+                  theme_minimal()
 
 
-## Check the vaccination rate is monotonic
-all(df == cummax(df$`2023-03-09`))))
+## Compare two states
+plot_2 <- df_byMonth %>%
+            filter(Province_State %in% c("Connecticut", "Kansas")) %>%
+            ggplot(data = ., aes(x = month, y = People_fully_vaccinated)) +
+                  geom_line(aes(color = Province_State), size = 1) +
+                  scale_y_continuous(labels = scales::comma) +
+                  labs(title = "People Fully Vaccinated\nConnecticut vs. Kansas",
+                       x = "Month",
+                       y = "People Fully Vaccinated") +
+                  theme_minimal()
 
 
-
-
+## Save a plot as a jpeg file
+plot_1 %>% ggsave("plot.jpeg", ., width = 10, height = 6, units = "in", dpi = 300)
 
 
 
