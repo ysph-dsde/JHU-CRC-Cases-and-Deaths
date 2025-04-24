edb47da3016ee691123dc9bada3c33c3c9956dab
Shelby Golden <sgolde13@alumni.jh.edu>
2025-04-24 01:31:27 -0400

    Save analysis changes

--- Analysis Script_Cases and Deaths Plot.R
+++ Analysis Script_Cases and Deaths Plot.R
@@ -76,6 +76,71 @@ covid19_death_processed <- covid19_death_raw %>%
   
 
 
+## ----------------------------------------------------------------------------
+## SHELBY'S STRINGR EXAMPLE
+
+'%!in%' <- function(x,y)!('%in%'(x,y))
+library(stringr)
+
+## Need to adjust the data cleaning so that the string columns are retained.
+
+df <- covid19_death_raw %>%
+  # Reshape data from wide to long format, with dates as a single column
+  pivot_longer(cols = "1/22/20":"3/9/23",
+               names_to = "date",
+               values_to = "cumulative_count") %>% as.data.frame()
+
+
+## We only need to inspect the "Province_State" entries. We expect that the
+## U.S. states and territories will be included. We confirm by matching
+## the unique entries of "Province_State" and datasets::state.name.
+
+unique(df$Province_State) %>% .[. %!in% datasets::state.name]
+
+
+## They included entries for the two cruise ships. We do not need to consider
+## these so we use str_detect() to remove rows where this information is
+## included. Two methods using stringr are shown:
+
+# Option #1: Use the Boolean test that detects the "Princess" string.
+df_filtered <- df[!str_detect(df$Province_State, "Princess"), ]
+
+# Option #2: Find the index that detects the "Princess" string and use the
+#            indexes that do not contain that string to subset.
+df_filtered <- df[str_which(df$Province_State, "Princess", negate = TRUE), ]
+
+
+head(df_filtered)
+
+## The "Combined_Key" variable combines the "Admin2", "Province_State", and
+## "Country_Region". We want to generate a new column that does not include
+## "Admin2". Two methods using stringr are shown:
+
+# Option #1: Generate a new column by combining the desired columns with ", "
+#            as the separator.
+df_filtered$Combined_Key_2 <- str_c(df_filtered$Province_State, 
+                                    df_filtered$Country_Region, sep = ", ")
+
+# Option #2: Split the string only to the first observation of the string match.
+df_filtered$Combined_Key_2 <- str_split(df_filtered$Combined_Key, ", ", 
+                                        simplify = TRUE, n = 2)[, 2]
+
+
+
+df_filtered$Combined_Key %>% str_split(., ", ", simplify = TRUE) %>%
+  str_c(.[, 2], .[, 3], ", ")
+
+
+
+df_filtered$combined_key_2 <- str_c(df_filtered$Province_State, df_filtered$Country_Region, sep = ", ")
+
+
+unique(df_filtered$Province_State)
+
+
+# str_replace
+
+
 
 ## ----------------------------------------------------------------------------
 ## DATA VISUALIZATION
